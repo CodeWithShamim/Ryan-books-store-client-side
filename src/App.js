931ff25aa758ca878b49d45225cd722d9ca5c781
codewithshamim <shamimislamonline@gmail.com>
2022-04-29 16:34:56 +0600

    inventory component added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignupForm from "./components/Login/SignupForm/SignupForm";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 import NotFound from "./components/Shared/NotFound/NotFound";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/login" element={<LoginForm></LoginForm>}></Route>
         <Route path="/signup" element={<SignupForm></SignupForm>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
