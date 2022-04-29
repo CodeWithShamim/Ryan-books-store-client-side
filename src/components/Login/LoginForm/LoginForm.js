92a1fd53923040cf7945d47f67964ff402447eb1
codewithshamim <shamimislamonline@gmail.com>
2022-04-29 20:16:53 +0600

    sign up form added

--- src/components/Login/LoginForm/LoginForm.js
+++ src/components/Login/LoginForm/LoginForm.js
@@ -7,25 +7,42 @@ const LoginForm = () => {
     e.preventDefault();
   };
   return (
-    <div className="login-container w-50 mx-auto">
-      <h2>Login</h2>
+    <div className="main-container">
+      <div className="login-container w-50 mx-auto">
+        <h2>Login</h2>
 
-      <form onSubmit={handleLogin} className="d-flex flex-column w-75 mx-auto">
-        <input className="mb-3" type="email" name="email" id="email" />
-        <input className="mb-3" type="password" name="password" id="password" />
-        <p className="">
-          Don't have an account?{" "}
-          <Link className="text-primary" to="/signup">
-            sign up
-          </Link>
-        </p>
-        <input type="submit" value="Login" />
-        <p>
-          <Link className="text-primary" to="/signup">
-            Forgot your password?
-          </Link>
-        </p>
-      </form>
+        <form
+          onSubmit={handleLogin}
+          className="d-flex flex-column w-75 mx-auto"
+        >
+          <input
+            className="mb-3"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email here"
+          />
+          <input
+            className="mb-3"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password here"
+          />
+          <p className="">
+            Don't have an account?{" "}
+            <Link className="text-primary" to="/signup">
+              sign up
+            </Link>
+          </p>
+          <input type="submit" value="Login" />
+          <p>
+            <Link className="text-primary" to="/signup">
+              Forgot your password?
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Login/SignupForm/SignupForm.js
+++ src/components/Login/SignupForm/SignupForm.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>This is SignupForm</h1>
+    <div className="main-container">
+      <div className="login-container w-50 mx-auto">
+        <h2>Register</h2>
+
+        <form
+          onSubmit={handleRegister}
+          className="d-flex flex-column w-75 mx-auto"
+        >
+          <label className="text-start" htmlFor="name">
+            Name
+          </label>
+          <input className="mb-3" type="text" name="name" id="name" />
+
+          <label className="text-start" htmlFor="email">
+            Email
+          </label>
+          <input className="mb-3" type="email" name="email" id="email" />
+
+          <label className="text-start" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="mb-3"
+            type="password"
+            name="password"
+            id="password"
+          />
+          <p className="">
+            Already have an account?{" "}
+            <Link className="text-primary" to="/login">
+              login
+            </Link>
+          </p>
+          <input type="submit" value="Register" />
+        </form>
+      </div>
     </div>
   );
 };
-
 export default SignupForm;
