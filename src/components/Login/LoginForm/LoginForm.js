d430de36763a976545dbde4e36fb8d3b6d6a5ddc
codewithshamim <shamimislamonline@gmail.com>
2022-04-29 16:52:26 +0600

    login form added

--- src/components/Login/LoginForm/LoginForm.js
+++ src/components/Login/LoginForm/LoginForm.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./LoginForm.css";
 
 const LoginForm = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>This is LoginForm</h1>
+    <div className="login-container w-50 mx-auto">
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin} className="d-flex flex-column w-75 mx-auto">
+        <input className="mb-3" type="email" name="email" id="email" />
+        <input className="mb-3" type="password" name="password" id="password" />
+        <p className="">
+          Don't have an account?{" "}
+          <Link className="text-primary" to="/signup">
+            sign up
+          </Link>
+        </p>
+        <input type="submit" value="Login" />
+        <p>
+          <Link className="text-primary" to="/signup">
+            Forgot your password?
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
