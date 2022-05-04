c0f208d215edde111a648a868bbdf0fd4510e47b
codewithshamim <shamimislamonline@gmail.com>
2022-05-04 14:42:20 +0600

    inventory added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
-  const { img, name, description, author } = item;
+  const { img, name, description, price, quantity, suppiler } = item;
 
   // get items by id
   useEffect(() => {
@@ -15,12 +16,22 @@ const Inventory = () => {
   }, []);
 
   return (
-    <div>
+    <div className="">
       <div className="book-detail">
-        <img src={img} alt="book-img" />
-        <h5>{name}</h5>
-        <p>{description}</p>
-        <h6>{author}</h6>
+        <div className="book-detail-left-side">
+          <img src={img} alt="book-img" className="w-50" />
+        </div>
+        <div className="book-detail-right-side">
+          <h5>{name}</h5>
+          <p>{description}</p>
+          <h5>
+            Price: <span>{price}</span>
+          </h5>
+          <h5>
+            Quantity: <span>{quantity}</span>
+          </h5>
+          <h6>{suppiler}</h6>
+        </div>
       </div>
       <Link to="/manage-inventory">
         <button className="submit-btn">Manage inventory</button>
