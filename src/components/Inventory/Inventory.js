04c58e6d1ddc98e532d8b6cef5fc06303d62d9f5
codewithshamim <shamimislamonline@gmail.com>
2022-05-04 19:02:54 +0600

    delivered btn event hanlder setup

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -6,6 +6,8 @@ const Inventory = () => {
   const { id } = useParams();
   const [item, setItem] = useState({});
   const { img, name, description, price, quantity, suppiler } = item;
+  // add new quantity hooks
+  const [newQuantity, setNewQuantity] = useState(0);
 
   // get items by id
   useEffect(() => {
@@ -13,6 +15,20 @@ const Inventory = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => setItem(data));
+  }, []);
+
+  // ---handle delivered---
+  const handleDelivered = () => {
+    if (quantity) {
+      const newQuantity = quantity - 1;
+      setNewQuantity(newQuantity);
+    }
+  };
+  console.log(newQuantity);
+
+  // ---update quantity---
+  useEffect(() => {
+    fetch;
   }, []);
 
   return (
@@ -28,7 +44,7 @@ const Inventory = () => {
           <h4>ADD QUANTITY</h4>
           <input type="text" name="restock-quantity" id="restock-quantity" />
           <input
-            className="w-50 m-1 bg-transparent border border-light text-light fw-bold"
+            className="w-75 m-1 bg-transparent border border-light text-light fw-bold"
             type="submit"
             value="Restock item"
           />
@@ -36,18 +52,23 @@ const Inventory = () => {
 
         {/* --- left side ---  */}
         <div className="book-detail-left-side m-2">
-          <h5>{name}</h5>
+          <h2>{name}</h2>
           <p className="w-75 mx-auto">{description}</p>
           <h5>
-            Price: <span>{price}</span>
+            Price: <span className="text-danger fw-bold">{price}</span>
           </h5>
           <h5>
-            Quantity: <span>{quantity}</span>
+            Quantity:{" "}
+            <span className="text-danger fw-bold fs-4">
+              {newQuantity ? newQuantity : quantity}
+            </span>
           </h5>
-          <h6>{suppiler}</h6>
+          <h6>Supplier: {suppiler}</h6>
 
           <div className="btn-box">
-            <button className="delivered-btn">Delivered</button>
+            <button onClick={handleDelivered} className="delivered-btn">
+              Delivered
+            </button>
             <Link to="/manage-inventory">
               <button className="manage-inventory-btn">Manage inventory</button>
             </Link>
