06bb685aa750719ee12c9fd35dfaa95c66996bdd
codewithshamim <shamimislamonline@gmail.com>
2022-05-05 18:51:18 +0600

    successfully get item by email from db

--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 import "./MyItems.css";
 
 const MyItems = () => {
+  const [user] = useAuthState(auth);
+  const [myItems, setMyItems] = useState([]);
+
+  // ---get item by email---
+  useEffect(() => {
+    const url = `http://localhost:5000/getItemByEmail?email=${user.email}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMyItems(data));
+  }, []);
+
   return (
     <div>
       <h1>this is MyItems</h1>
+      {myItems.map((myItem) => (
+        <h4>{myItem.name}</h4>
+      ))}
     </div>
   );
 };
