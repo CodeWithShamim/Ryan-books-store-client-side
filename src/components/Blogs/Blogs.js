92853a14f971684eb750e7033cdc8af1e2fca1a9
codewithshamim <shamimislamonline@gmail.com>
2022-05-07 14:20:53 +0600

    blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "./Blogs.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>This is Blogs........</h1>
+    <div className="blogs-container">
+      <div className="blog blog-1">
+        <h3>Difference between javascript and nodejs</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia amet
+          cum, commodi ad omnis, nam dolores deserunt ullam quia quo, vel soluta
+          non ut tempora vero expedita tempore natus. Quaerat!
+        </p>
+      </div>
+      <div className="blog blog-2">
+        <h3>When should you use nodejs and when should you use mongodb</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          illo est accusantium iure repellat tempore dolores facere ratione
+          necessitatibus dolor hic. Quidem mollitia, saepe deleniti aspernatur
+          facere unde pariatur quos?
+        </p>
+      </div>
+      <div className="blog blog-3">
+        <h3>Differences between sql and nosql databases.</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          illo est accusantium iure repellat tempore dolores facere ratione
+          necessitatibus dolor hic. Quidem mollitia, saepe deleniti aspernatur
+          facere unde pariatur quos?
+        </p>
+      </div>
+      <div className="blog blog-4">
+        <h3>What is the purpose of jwt and how does it work</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          illo est accusantium iure repellat tempore dolores facere ratione
+          necessitatibus dolor hic. Quidem mollitia, saepe deleniti aspernatur
+          facere unde pariatur quos?
+        </p>
+      </div>
     </div>
   );
 };
