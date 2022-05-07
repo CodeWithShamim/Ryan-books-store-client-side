b64e5de8db07d7b3b08918a8294e0cbf892eb04e
codewithshamim <shamimislamonline@gmail.com>
2022-05-07 12:57:30 +0600

    jwt auth added

--- src/hooks/useMyItems.js
+++ src/hooks/useMyItems.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
 import auth from "../firebase.init";
 
 const useMyItems = () => {
@@ -12,17 +14,32 @@ const useMyItems = () => {
         setLoader(true);
         // const url = `https://ryan-books-store.herokuapp.com/getItemByEmail?email=${user?.email}`;
         const url = `http://localhost:5000/getItemByEmail?email=${user?.email}`;
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
+        const loadMyItems = async() => {
+            try {
+                const { data } = await axios(url, {
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
                 setLoader(false);
                 setMyItems(data);
-            });
+            } catch (error) {
+                if (error.response.status === 403) {
+                    toast.error("Forbidden access")
+                }
+                if (error.response.status === 401) {
+                    toast.error("Unauthorized access")
+                }
+            }
+
+        }
+
+        loadMyItems();
 
     }, [user]);
 
-    console.log(user)
-    console.log(myItems)
+    // console.log(user)
+    // console.log(myItems)
     return [myItems, setMyItems, loader]
 
 
