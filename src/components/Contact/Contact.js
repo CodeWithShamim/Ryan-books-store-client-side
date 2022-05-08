31aa4408e86798c70a2ed195fa27fc9035b2f113
codewithshamim <shamimislamonline@gmail.com>
2022-05-08 20:47:56 +0600

    contact page updated

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,172 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>this is Contact</h1>
+    <div className="contact-container">
+      <section className="ftco-section">
+        <div className="container">
+          <div className="row justify-content-center"></div>
+          <div className="row justify-content-center">
+            <div className="col-md-12">
+              <div className="wrapper">
+                <div className="row no-gutters mb-5">
+                  <div className="col-md-7">
+                    <div className="contact-wrap w-100 p-md-5 p-4">
+                      <h3 className="mb-4">Contact Us</h3>
+                      <div id="form-message-warning" className="mb-4"></div>
+                      <div id="form-message-success" className="mb-4">
+                        Your message was sent, thank you!
+                      </div>
+                      <form
+                        method="POST"
+                        id="contactForm"
+                        name="contactForm"
+                        className="contactForm"
+                      >
+                        <div className="row">
+                          <div className="col-md-6">
+                            <div className="form-group">
+                              <label className="label text-start" for="name">
+                                Full Name
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                id="name"
+                                placeholder="Name"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-md-6">
+                            <div className="form-group">
+                              <label className="label text-start" for="email">
+                                Email Address
+                              </label>
+                              <input
+                                type="email"
+                                className="form-control"
+                                name="email"
+                                id="email"
+                                placeholder="Email"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <label className="label text-start" for="subject">
+                                Subject
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                name="subject"
+                                id="subject"
+                                placeholder="Subject"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <label className="label text-start" for="#">
+                                Message
+                              </label>
+                              <textarea
+                                name="message"
+                                className="form-control"
+                                id="message"
+                                cols="30"
+                                rows="4"
+                                placeholder="Message"
+                              ></textarea>
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <input
+                                type="submit"
+                                value="Send Message"
+                                className="btn btn-primary"
+                              />
+                              <div className="submitting"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="col-md-5 d-flex align-items-stretch">
+                    <div id="map">
+                      <img
+                        src="https://i.postimg.cc/Jz2Kcp0J/download-1.jpg"
+                        alt="map-img"
+                        className="img-fluid p-5"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-3">
+                    <div className="dbox w-100 text-center">
+                      <div className="icon d-flex align-items-center justify-content-center">
+                        <span className="fa fa-map-marker"></span>
+                      </div>
+                      <div className="text">
+                        <p>
+                          <span>Address:</span> 198 West 21th Street, Suite 721
+                          New York NY 10016
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3">
+                    <div className="dbox w-100 text-center">
+                      <div className="icon d-flex align-items-center justify-content-center">
+                        <span className="fa fa-phone"></span>
+                      </div>
+                      <div className="text">
+                        <p>
+                          <span>Phone:</span>{" "}
+                          <a href="tel://1234567920">+ 1235 2355 98</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3">
+                    <div className="dbox w-100 text-center">
+                      <div className="icon d-flex align-items-center justify-content-center">
+                        <span className="fa fa-paper-plane"></span>
+                      </div>
+                      <div className="text">
+                        <p>
+                          <span>Email:</span>{" "}
+                          <a href="mailto:info@yoursite.com">
+                            info@yoursite.com
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3">
+                    <div className="dbox w-100 text-center">
+                      <div className="icon d-flex align-items-center justify-content-center">
+                        <span className="fa fa-globe"></span>
+                      </div>
+                      <div className="text">
+                        <p>
+                          <span>Website</span> <Link to="">yoursite.com</Link>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
